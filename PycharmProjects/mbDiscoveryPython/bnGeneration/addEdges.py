899444ca6bebfe02b150286929eea4ebeda041b1
kelvinyangli <yang.kelvinli@monash.edu>
2016-05-27 10:58:24 +1000

    update

--- PycharmProjects/mbDiscoveryPython/bnGeneration/addEdges.py
+++
@@ -1,7 +0,0 @@
-def addEdges(graph, edges):
-    for e in edges:
-        if isinstance(e[0], tuple):
-            graph.edge(*e[0], **e[1])
-        else:
-            graph.edge(*e)
-    return graph
--- PycharmProjects/mbDiscoveryPython/bnGeneration/addNodes.py
+++
@@ -1,8 +0,0 @@
-
-def addNodes(graph, nodes):
-    for n in nodes:
-        if isinstance(n, tuple):
-            graph.node(n[0], **n[1])
-        else:
-            graph.node(n)
-    return graph
