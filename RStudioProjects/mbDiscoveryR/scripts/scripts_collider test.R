a5a9fda585d9e5021eca5355eb789e89d593545d
kelvinyangli <yang.kelvinli@monash.edu>
2016-11-25 17:57:03 +1100

    update@pc

--- RStudioProjects/mbDiscoveryR/scripts/scripts_collider test.R
+++ RStudioProjects/mbDiscoveryR/scripts/scripts_collider test.R
@@ -9,22 +9,22 @@ graphviz.plot(dag)
 
 allNodes = bnlearn::nodes(dag)
 X = "V2"
-Y = "V3"
-Z = "V4"
+Y = "V4"
+Z = "V6"
 xIndex = which(allNodes == X)
 yIndex = which(allNodes == Y)
 zIndex = which(allNodes == Z)
 
-n = 100
-cmicount = mmlcount = 0 
-for (i in 1:1000) {
+n = 1000
+cmicount = mmlcount = rep(0, 3) 
+for (i in 1:100) {
   cpts = generateCPTs(dag, 2, 1)
   data = rbn(cpts, n)
   dataInfo = getDataInfo(data)
   
   # x ->z<- y
   len1 = mmlCPT(xIndex, c(), dataInfo$indexListPerNodePerValue, dataInfo$arities, n) + 
-    mmlCPT(yIndex, c(), dataInfo$indexListPerNodePerValue, dataInfo$arities, n) + 
+    mmlCPT(yIndex, c(xIndex), dataInfo$indexListPerNodePerValue, dataInfo$arities, n) + 
     mmlCPT(zIndex, c(xIndex, yIndex), dataInfo$indexListPerNodePerValue, dataInfo$arities, n)
   
   cmi1 = (ci.test(X, Y, Z, data = data)$statistic - ci.test(X, Y, data = data)$statistic)/ci.test(X, Y, data = data)$statistic
@@ -45,10 +45,26 @@ for (i in 1:1000) {
   
   #cat("z collider:", len1, "-- y collider:", len2, "-- z collider", len3, "\n")
   
-  if (which.min(c(len1, len2, len3)) == 1) mmlcount = mmlcount + 1
-  if (which.max(c(cmi1, cmi2, cmi3)) == 1) cmicount = cmicount + 1
-  
+  indexmml = which.min(c(len1, len2, len3))
+  indexcmi = which.max(c(cmi1, cmi2, cmi3))
+  mmlcount[indexmml] = mmlcount[indexmml] + 1
+  cmicount[indexcmi] = cmicount[indexcmi] + 1
+
 }
 
-print(mmlcount)
-print(cmicount)
+(mmlcount)
+(cmicount)
+
+
+for (n in 1:10) cat("n =", n, ": #DAGs =", nDags(n), "-- #MBDAGs =", nMBDags(n - 1), "\n")
+
+
+
+
+
+
+
+
+
+
+
