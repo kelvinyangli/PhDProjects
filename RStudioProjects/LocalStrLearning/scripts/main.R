61808ed9fcf3bdc4e65974445577f09978849bcb
kelvinyangli <yang.kelvinli@monash.edu>
2017-02-14 22:46:56 +1100

    update@mac

--- RStudioProjects/LocalStrLearning/scripts/main.R
+++ RStudioProjects/LocalStrLearning/scripts/main.R
@@ -6,53 +6,43 @@
 # polytree, we may want to compare our method with polytree learning algorithm without
 # considering true and false negatives.
 
-adjmtx = randAdjmtx(12, 3)
+adjmtx = randAdjmtx(9, 3)
 dag = matrix2dag(adjmtx)
 graphviz.plot(dag)
 cpts = randCPTs(dag, 2, 1)
-<<<<<<< HEAD
-data = rbn(cpts, 1000)
-
-# extract mb(x) from the true model
-x = "V6"
-mbVars = mBlkt(adjmtx, x)
-n = length(mbVars) # size of mb
-mbpts = readRDS(paste0("MBPTs/", n, ".rds")) # load pre-saved mbpts for mb size n
-mbpts = substituteVar(mbpts, x, mbVars) # replace default vars with mb vars
-
-# a function to extract the bn of a var and its local str
-=======
 n = 1000
 data = rbn(cpts, n)
-vars = colnames(adjmtx) # all vars
+dataInfo = getDataInfo(data)
+vars = colnames(adjmtx)
 for (i in 1:length(vars)) { # iterate through all vars 
   
   target = vars[i]
-  
+  cat(target, ": \n")
   mbVars = mBlkt(adjmtx, target) # extract mb(x) from the true model
   mbpts = readRDS(paste0("MBPTs/", length(mbVars), ".rds")) # load pre-saved mbpts for mb size n
   mbpts = substituteVar(mbpts, target, mbVars) # replace default vars with mb vars
-  dataInfo = getDataInfo(data)
   mmlmtx = computeMMLMatrix(vars, mbVars, target, dataInfo, n) # compute mmlcpt for each node in mbVars given its possible parents
-  
   # compute mmlcpt for each mbpt 
   scores = rep(0, length(mbpts))
-  for (i in 1:length(mbpts)) scores[i] = mmlDag_fast(mbpts[[i]], vars, dataInfo, mmlmtx, n)
+  for (j in 1:length(mbpts)) scores[j] = mmlDag_fast(mbpts[[j]], vars, dataInfo, mmlmtx, n)
   index = which.min(scores) # find the minimum score's index
   mbptLearned = mbpts[index][[1]][c(target, mbVars),c(target,mbVars)] # the learned mbpt
   mbTrue = adjmtx[c(target, mbVars),c(target,mbVars)] # extract local str within mb(x) and compare with the learned mbpt
-  
+  print(mbptLearned)
+  cat("---------------- \n")
   # compare the learnd mbpt with the true mb
   # at the moment we don't care about false negatives, because mbpt is a sub-graph of mb's local str
   # we only care about false positives, i.e., those arcs that are learned but not in the true local str
-  acc = strAccuracy(mbTrue, mbptLearned)
+  (acc = strAccuracy(mbTrue, mbptLearned))
+  (precision = (sum(mbptLearned) - acc$delete - acc$reverse) / sum(mbptLearned))
+  (recall = (sum(mbptLearned) - acc$delete - acc$reverse) / sum(mbTrue))
   
 }
 
-
-
->>>>>>> master
-
+# visulize learned mbpt and the true mb
+par(mfrow = c(1, 2))
+graphviz.plot(matrix2dag(mbLocalStr(adjmtx, vars, vars[i], mbVars)), main = "true")
+graphviz.plot(matrix2dag(mbptLearned), main = "learned")
 # once the optimal mbpt is learned, we use this result as the start of the next step
 # which is completing the remaining arcs.
 # this can be achieved in various ways, but the most straightfoward method is to 
