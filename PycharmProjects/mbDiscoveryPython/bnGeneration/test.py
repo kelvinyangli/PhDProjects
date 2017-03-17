2397fa7da6833b7fe183c913e5ef48e682052303
kelvinyangli <yang.kelvinli@monash.edu>
2017-03-17 18:04:58 +1100

    mac

--- PycharmProjects/mbDiscoveryPython/bnGeneration/test.py
+++ PycharmProjects/mbDiscoveryPython/bnGeneration/test.py
@@ -1,16 +1,8 @@
-from generateBN import *
 
 
-dag = generateDag(15, 3)
-cpts = generateCPTs(dag, 2, 1)
-data = generateData(dag, cpts, 1000)
+def readfile(filename):
+    f = open(filename, 'r')
+    print(f)
 
-print(dag.nodes())
-
-tempDag = dag
-tempDag.layout(prog="dot")
-tempDag.draw("C:\PhDProjects\PycharmProjects\mbDiscoveryPython\img\dag.png")
-
-print(tempDag.nodes())
-# print(cpts)
-print(data.iloc[0:10,])
+#pwd
+readfile('../test.txt')
