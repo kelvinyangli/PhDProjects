31a01a6f0ca905b301bd6bf3bc129a2d6424f459
kelvinyangli <yang.kelvinli@monash.edu>
2017-03-18 23:07:43 +1100

    format_prior

--- RStudioProjects/LocalStrLearning/scripts/format_camml_prior.R
+++ RStudioProjects/LocalStrLearning/scripts/format_camml_prior.R
@@ -1,8 +1,9 @@
 # this script format the optimal local polytree structures learned into the
 # required format for camml prior
+fixed = TRUE
 nVars = 20
-n = 1000
-dir = "../../../Dag experiments/"
+n = 100
+dir = "../../../UAI_exp/"
 mbptsList = list()
 for (i in 1:8) mbptsList[[i]] = readRDS(paste0("MBPTs/", i - 1, ".rds")) 
 
@@ -11,9 +12,9 @@ logFactorialSheet = read.csv("logFactorial_1to10000.csv")
 
 data_sets = list.files(paste0(dir, "data_csv/", n, "/"))
 maxMB = 7
-nResamples = 10
-maxProb = 0.9
-Rprof("prior.out")
+nResamples = 5
+maxProb = 0.8
+#Rprof("prior.out")
 for (i in 1:length(data_sets)) {
 #for (i in 2:2) {
   filename = strsplit(data_sets[i], ".csv")[[1]][1]
@@ -30,14 +31,21 @@ for (i in 1:length(data_sets)) {
   
   mtx = mergeMBPTs(localStrs, vars) # merge local pts
   arcs = extractArcs(mtx) # get arcs
-  #priors = list(directed = rep(0.8, length(arcs$directed)/2), undirected = rep(0.8, length(arcs$undirected)/2))
-  priors = arcPrior(arcs, maxProb, mbList, localStrs, mbptsList, vars, data, nResamples) # sample priors
+  if (fixed) {
+    priors = list(directed = rep(maxProb, length(arcs$directed)/2), undirected = rep(0.8, length(arcs$undirected)/2))
+  } else {
+    priors = arcPrior(arcs, maxProb, mbList, localStrs, mbptsList, vars, data, nResamples) # sample priors
+  }
   text = cammlPrior(arcs, priors)
-  write_file(text, paste0(dir, "prior_unfixed/", filename, ".txt"))
+  if (fixed) {
+    write_file(text, paste0(dir, "prior_fixed/", filename, ".txt"))
+  } else {
+    write_file(text, paste0(dir, "prior_unfixed/", filename, ".txt"))
+  }
   
 }
-Rprof(NULL)
-proftable("prior.out")
+#Rprof(NULL)
+#proftable("prior.out")
 
 
 
