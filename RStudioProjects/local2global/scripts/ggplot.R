cef06967f849a2571a7483355e66f9d9e42ea0db
kelvinyangli <yang.kelvinli@monash.edu>
2017-04-01 21:01:17 +1100

    pc update

--- RStudioProjects/local2global/scripts/ggplot.R
+++ RStudioProjects/local2global/scripts/ggplot.R
@@ -1,60 +1,48 @@
-<<<<<<< HEAD
 #require(reshape2)
 #library(ggplot2)
 
-#ed = read.csv("../../../UAI_exp/plot_sample.csv")
-#ed$Sample = as.factor(ed$Sample)
-str(ed)
-pd <- position_dodge(width = 0.2)
-ggplot(ed, aes(x = Sample, y = Mean, fill = Method)) + 
-  geom_point(aes(color = Method, shape = Method), size = 1.5, position = pd) +
-  geom_errorbar(aes(ymin = Mean - CI, ymax = Mean + CI),
-                width = 0.1, position = pd) + 
-  facet_wrap(~Network)
-=======
-#require(ggplot2)
-#require(GGally)
-#require(reshape2)
-#require(lme4)
-#require(compiler)
-#require(parallel)
-#require(boot)
+
+models = c("ALARM", "ALARM3", "CHILD", "CHILD3", "BARLEY", "INSURANCE", "INSURANCE3", "INSURANCE5")
+#models = c("INSURANCE", "INSURANCE3", "INSURANCE5")
+measureMB = TRUE
+if (!measureMB) {
+  data = read.csv("../../../UAI_exp/plot1.csv")
+  data$Method = factor(data$Method, levels = c("MBPT", "SLL+C", "SLL+G", "MMHC", "GES", "CaMML"), 
+                       labels = c("MBPT", "SLL+C", "SLL+G", "MMHC", "GES", "CaMML"))  
+} else {
+  data = read.csv("../../../UAI_exp/plot2.csv")
+  data$Method = factor(data$Method, levels = c("MBPT", "SLL", "HITON"), labels = c("MBPT", "SLL", "HITON"))  
+}
+data$Sample = as.factor(data$Sample)
+
+for (i in 1:length(models)) {
+  if (!measureMB) {
+    ggplot(data[which(data$Network == models[i]),], aes(x = Sample, y = Edit_distance, fill = Method)) + 
+      geom_point(aes(color = Method, shape = Method), size = 3, position = position_dodge(width = 0.5)) +
+      geom_errorbar(aes(ymin = Edit_distance - CI, ymax = Edit_distance + CI),
+                    width = 0.1, position = position_dodge(width = 0.5)) +
+      labs(title = models[i]) + 
+      theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14), legend.key.size = unit(0.3, "in"))
+    filename = tolower(models[i])
+    ggsave(paste0("C:/Users/Administrator/Dropbox/PhD@Monash/Scaling-up polytree learning/UAI 2017/figures/", 
+                  filename, ".pdf"))
+  } else {
+    if (length(which(data$Network == models[i])) > 0) {
+      ggplot(data[which(data$Network == models[i]),], aes(x = Sample, y = False_finding, fill = Method)) + 
+        geom_point(aes(color = Method, shape = Method), size = 3, position = position_dodge(width = 0.5)) +
+        geom_errorbar(aes(ymin = False_finding - CI, ymax = False_finding + CI),
+                      width = 0.1, position = position_dodge(width = 0.5)) +
+        labs(title = models[i]) + 
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14), legend.key.size = unit(0.3, "in"))
+      filename = paste0(tolower(models[i]), "_mb")
+      ggsave(paste0("C:/Users/Administrator/Dropbox/PhD@Monash/Scaling-up polytree learning/UAI 2017/figures/", 
+                    filename, ".pdf"))
+      
+    }
+    
+  }
+  
+  
+}
 
 
-library(ggplot2)
-
-# create fake dataset with additional attributes - sex, sample, and temperature
-x <- data.frame(
-  values = c(runif(100, min = -2), runif(100), runif(100, max = 2), runif(100)),
-  sex = rep(c('M', 'F'), each = 100),
-  sample = rep(c('sample_a', 'sample_b'), each = 200),
-  temperature = sample(c('15C', '25C', '30C', '42C'), 400, replace = TRUE)
-)
-
-# compare different sample populations across various temperatures
-ggplot(x, aes(x = sample, y = values, fill = sex)) +
-  geom_boxplot() +
-  facet_wrap(~ temperature)
-
-
-x = c(41.1, 36.4, 37.1, 35.7, 34.8, 29, 16.4, 8.3, 14.6, 7.4, 9.7, 0, 84.5, 83.7, 79.2, 81.1, 69.9, 77.7, 88.5, 108.6, 90.1, 108.8, 90.85, 111.9)
-y = rep(c(500, 1000, 5000), 4)
-length(x)
-
-m = matrix(x, 12, 2, byrow = TRUE)
-m = cbind(c(rep("Insurance", 3), rep("Alarm", 3), rep("Barley", 3), rep("Hailfinder", 3)), y, m)
-m[,c(2,3,4)] = as.numeric(m[,c(2,3,4)])
-colnames(m) = c("Model", "Sample", "MBPT", "CaMML")
-
-
-df = rbind(m[,c(1,2,3)],m[,c(1,2,4)])
-df=cbind(df, c(rep("MBPT", 12), rep("CaMML", 12)))
-colnames(df) = c("Model", "Sample", "editDistance", "Algorithm")
-df = data.frame(df)
-df = df[,c(3, 4, 2, 1)]
-df[,1]=as.numeric(levels(df[,1]))[df[,1]]
-
-df = read.csv("../../../UAI_exp/ijcai.csv")
-df[, 2] = as.factor(df[,2])
-ggplot(df, aes(x = Sample, y = editDistance)) + geom_boxplot() + facet_wrap(~ Model)
->>>>>>> master
--- RStudioProjects/local2global/scripts/mb_accuracy.R
+++ RStudioProjects/local2global/scripts/mb_accuracy.R
@@ -1,30 +1,35 @@
-model = "insurance3"
+model = "barley"
 dir = paste0("../../../UAI_exp/", model, "/")
 n = 5000
 nRepeat = 10
 dag = readRDS(paste0(dir, "dag/", model, ".rds"))
 vars = bnlearn::nodes(dag)
-mtx = matrix(0, nRepeat * length(vars), 4)
+#mtx = matrix(0, nRepeat * length(vars), 4)
 (mbs = list.files(paste0(dir, "mb/", n)))
-k = 1
+#k = 1
+#v = c()
+w = c()
 for (i in 1:length(mbs)) {
   mbList = readRDS(paste0(dir, "mb/", n, "/", mbs[i]))
+  v = c()
   for (j in 1:length(mbList)) {
-    res = mbAccuracy(bnlearn::mb(dag, vars[j]), mbList[[j]], vars[j], vars)
-    mtx[k, 1] = res[[1]]
-    mtx[k, 2] = res[[2]]
-    mtx[k, 3] = sqrt((1 - res[[1]])^2 + (1 - res[[2]])^2)
-    if ((mtx[k,1]==0) && (mtx[k,2]==0)) {
-      mtx[k,4]=0
-    } else {
-      mtx[k, 4] = 2 * res[[1]] * res[[2]] / (res[[1]] + res[[2]])  
-    }
+    res = mbEditDist(bnlearn::mb(dag, vars[j]), mbList[[j]], vars[j], vars)
+    v = c(v, res)
+    #mtx[k, 1] = res[[1]]
+    #mtx[k, 2] = res[[2]]
+    #mtx[k, 3] = sqrt((1 - res[[1]])^2 + (1 - res[[2]])^2)
+    #if ((mtx[k,1]==0) && (mtx[k,2]==0)) {
+    #  mtx[k,4]=0
+    #} else {
+    #  mtx[k, 4] = 2 * res[[1]] * res[[2]] / (res[[1]] + res[[2]])  
+    #}
     
-    k = k + 1
+    #k = k + 1
   }
+  w = c(w, sum(v))
 } # end for i
 cat(model, "-", n, "\n")
-cat(round(colMeans(mtx),2), "\n")
-cat(round(1.96*apply(mtx,2,sd)/sqrt(nrow(mtx)),2))
+cat(round(mean(w),2), "\n")
+cat(round(1.96*sd(w)/sqrt(length(w)),2))
 
-  
+
