6fa3f60f22f651f57ec62cfdab74c917d60e5917
kelvinyangli <yang.kelvinli@monash.edu>
2017-11-14 17:14:27 +1100

    fixed bug on mml_nb_adaptive
    using cond ind assumpt now

--- RStudioProjects/lglbnlearn/R/mml_nb_adaptive.R
+++ RStudioProjects/lglbnlearn/R/mml_nb_adaptive.R
@@ -6,47 +6,85 @@
 #' from MML87 for NB. But maybe this is ok, because we are not interested in the mml estimation of parameters. 
 #' @param data A categorical data set.
 #' @param arities A vector of variable arities. 
-#' @param yIndex The target variable index.
-#' @param xIndices A vector of indices for the Xs. 
+#' @param paIndex The target variable (or parent node) index.
+#' @param chIndices A vector of indices for the Xs (or child nodes). 
 #' @export
-mml_nb_adaptive = function(data, arities, yIndex, xIndices) {
-  msgLen = 0
-  # initialize variable count with 1 to avoid having 0 count
-  # other values are also possible
-  ycnt = rep(1, nlevels(data[, yIndex])) # count for each value of y
-  # count for each value of x given each value of y, hence it is the cross 
-  # product of arities of y and x
-  if (length(xIndices) > 0) xcnt = rep(1, prod(arities[c(xIndices, yIndex)]))
-  # update variable count on each data point
-  for (rowID in 1:nrow(data)) {
-    yValue = as.numeric(data[rowID, yIndex])
-    p = ycnt / sum(ycnt) # marginal probability of y i.e., p(y)
-    if (length(xIndices) > 0) {# if there is at least one x
-      xValues = as.numeric(data[rowID, xIndices])
-      q = rep(0, length(xcnt)) # joint distribution p(x, y)
-      probX = 0
-      # update p(x, y) for each value of y
-      for (i in 1:arities[yIndex]) {
-        q[((i - 1) * prod(arities[xIndices]) + 1):(prod(arities[xIndices]) * i)] = 
-          (xcnt[((i - 1) * prod(arities[xIndices]) + 1):(prod(arities[xIndices]) * i)] / 
-             sum(xcnt[((i - 1) * prod(arities[xIndices]) + 1):(prod(arities[xIndices]) * i)])) * p[i]
-        
-        index = node_value_to_index(arities[c(xIndices, yIndex)], c(xValues, i))
-        # marginalize the p(x, y) for all y to get p(x)
-        probX = probX + q[index] 
-        # if y's ith value matches its current value then cach the current 
-        # combination index
-        if (i == yValue) cachedIndex = index
-      }# end for each value of y
-      msgLen = msgLen - log(q[cachedIndex] / probX) # upate message length
-      xcnt[cachedIndex] = xcnt[cachedIndex] + 1 # update the corresponding count x|y by 1
-    } else {# if there is no x
-      msgLen = msgLen - log(p[yValue]) 
+mml_nb_adaptive = function(data, arities, paIndex, chIndices) {
+  
+  msgLen = 0 
+  paCnt = rep(1, nlevels(data[, paIndex])) # parent value count
+  
+  # a list to store children count, the reason a list is used is because different
+  # child nodes could have different arities
+  if (length(chIndices) > 0) {
+    
+    chCnt = list()
+    for (j in 1:length(chIndices)) {
+      
+      chCnt[[j]] = rep(1, prod(arities[c(paIndex, chIndices[j])]))
+      
     }
-    ycnt[yValue] = ycnt[yValue] + 1 # update count y by 1
-  }# end of each data point
+    
+  }
+  
+  for (rowID in 1:nrow(data)) {
+    
+    paValue = as.numeric(data[rowID, paIndex])
+    p = paCnt / sum(paCnt) # marginal distribution p(paNode)
+    
+    if (length(chIndices) > 0) {
+      
+      chValues = as.numeric(data[rowID, chIndices])
+      probXCondY = chCnt # initialize prob(x_j | y)
+      probX = 0 # marginal probability p(x_1, x_2, ...)
+      for (i in 1:arities[paIndex]) {# for each value of the parent node
+        
+        probXJointY = p[i] # initialize p(x_j, y) with p(y)
+        for (j in 1:length(chIndices)) {# for each child node
+          
+          indices = ((i - 1) * prod(arities[chIndices[j]]) + 1):(prod(arities[chIndices[j]]) * i)
+          probXCondY[[j]][indices] = chCnt[[j]][indices] / sum(chCnt[[j]][indices]) # update p(x_j | y)
+          index = node_value_to_index(arities[c(chIndices[j], paIndex)], c(chValues[j], i))  
+          probXJointY = probXJointY * probXCondY[[j]][index]
+          if (i == paValue) {
+            
+            chCnt[[j]][index] = chCnt[[j]][index] + 1 # update count x|y by 1
+            #cat(unlist(chCnt), "\n")
+            
+          }
+          
+        }# end for each child node
+        
+        # if y's ith value matches its current value, then cach the joint probability p(x, y)
+        if (i == paValue) cachedValue = probXJointY
+        probX = probX + probXJointY # keep marginalizing p(x, y) to get p(x_1, x_2, ...)
+        
+      }# end for each value of the parent node
+      
+      probYCondX = cachedValue / probX
+      #cat(probYCondX, "\n")
+      msgLen = msgLen - log(probYCondX) # update message length
+      #cat(msgLen, "\n")
+      
+    } else {
+      
+      msgLen = msgLen - log(p[paValue])
+      #cat(p[paValue], "\n")
+      
+    }
+    
+    paCnt[paValue] = paCnt[paValue] + 1 # update count y by 1
+    #cat(paCnt, "\n")
+    
+  } # end for each row
+  
   return(msgLen)
+  
 }
+
+
+
+
 
 
 
